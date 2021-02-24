d7e7f39abaf0b5c2edb0d3b513f230420f20252f
source-rohanjoshi! <joshirohan9730@gmail.com>
2021-02-24 15:38:24 +0530

    pre or abs

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 
-echo 'welcome'
+isPresent=1;
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ];
+then
+	echo 'Employee is Present';
+else
+	echo 'Employee is Absent';
+fi
+
 
